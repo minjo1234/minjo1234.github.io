fac5c9318b2f391f6acbc301927bc046f47b1867
mjo <mjo@goodmit.co.kr>
2025-02-01 17:12:07 +0900

    profile_update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738393567',resources: [
+    cacheName: 'chirpy-1738397507',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
