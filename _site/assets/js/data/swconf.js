44fcbf9eaa3ccbf5634c550615625d9e668ee4a8
mjo <mjo@goodmit.co.kr>
2025-02-01 21:58:48 +0900

    image_upadte

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738414408',resources: [
+    cacheName: 'chirpy-1738414706',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
